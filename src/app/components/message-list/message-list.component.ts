3466302a7076dddacc99cbc50ac8da0ea63ef9af
[Sergi Bosque Ródenas] <[sbosquero@uoc.edu]>
2024-03-04 20:00:28 +0100

    Loading messages implemented

--- src/app/components/message-list/message-list.component.ts
+++ src/app/components/message-list/message-list.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MessageServiceService } from '../../services/message-service.service';
+import { MessageDTO } from '../../models/message.dto';
 
 @Component({
   selector: 'app-message-list',
   templateUrl: './message-list.component.html',
   styleUrl: './message-list.component.css'
 })
-export class MessageListComponent {
+export class MessageListComponent implements OnInit {
+  messages!: MessageDTO[];
+
+  constructor(private messageService: MessageServiceService){}
+
+  ngOnInit(): void {
+      this.loadMessages();
+  }
+
+  private async loadMessages(): Promise<void> {
+    try{
+      this.messages = await this.messageService.getMessages();
+    }catch(e: any){
+      this.messageService.errorLog(e);
+    }
+  }
 
 }
